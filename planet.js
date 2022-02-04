f9f49be2306fbf9cff2416579a53fb2706cef24c
antoine-overflow <antoine@DESKTOP-VKFREPH.localdomain>
2022-02-04 16:01:20 +0100

    satellite turning the way we want

--- planet.js
+++ planet.js
@@ -12,7 +12,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.SphereGeometry( 1, 32, 32 );
+var geometry = new THREE.SphereGeometry( 1, 64, 32 );
 
 var fond_carte = new THREE.TextureLoader().load( "planete.jpg" );
 var texture = new THREE.TextureLoader().load( "texture.jpg" );
@@ -24,6 +24,7 @@ var material = new THREE.MeshPhongMaterial( {
     bumpMap :  texture,
     bumpScale :0.05,
     side: THREE.DoubleSide,
+    wireframe: false
     //specularMap : new THREE.TextureLoader().load('lightness.jpg'),
     //specular  : new THREE.Color('grey')
 } );
@@ -36,22 +37,35 @@ material = new THREE.MeshLambertMaterial({
     color: 0x0992299});
 
 var stick = new THREE.Object3D();
-var point = new THREE.Vector3( 1, 1, 1 );
-stick.lookAt( point );
-terre.add( stick );
+
+var x=0; var y=0; var z=1.01; // coordinates where the satellite is above
+var point = new THREE.Object3D();
+point.translateZ(z);
+
+var target = new THREE.Vector3();
+point.getWorldPosition(target);
+
+stick.lookAt( target );
+terre.add( point );
 
 var geometrySat = new THREE.BoxGeometry( 0.11, 0.18, 0.06); // Parameters for the size of "satellite"
 var mesh = new THREE.Mesh( geometrySat, material );
-var r = 3; // Parameter for the distance from the earth
+var r = 1.5; // Parameter for the distance from the earth >1 for being in the sky
 mesh.position.set( 0, 0, r ); 
 stick.add( mesh );    
-
+point.add(stick);
 camera.position.z = 5;
 let controls = new THREE.OrbitControls( camera ,renderer.domElement);
 
 function render() {
     requestAnimationFrame( render);
     terre.rotation.y += 0.001;
+
+    var t = 0.005; // Variation of the satellite 
+    point.rotateX(t);
+    point.rotateY(t);
+    // point.rotateZ(t);
+
     controls.update();
     renderer.render( scene, camera );
 }
